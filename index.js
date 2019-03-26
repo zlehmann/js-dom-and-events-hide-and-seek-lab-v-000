c953c3f1d6e2b99d8066391194d5798ba49fc6c3
Zachary Lehmann <zlehmann@gmail.com>
2019-03-26 22:49:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div#nested div.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let nodes = document.querySelectorAll('#grand-node div');
+  if (!nodes.length) {
+    return false;
+  }
+  return nodes[nodes.length-1];
+}
