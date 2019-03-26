bb9d50a97563df1322c6975c4957fe9a2b0d2165
Zachary Lehmann <zlehmann@gmail.com>
2019-03-26 22:32:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('div#nested div.target');
 }
+
+function increaseRankBy(n) {
+  const lis = document
+    .getElementByID('app')
+    .querySelectorAll('ul.unranked-list li');
+
+  for (let i = 0; i <lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis.content) * n).toString();
+  }
+}
