9f13b36946abe3cee7e1244bf9c7618b05c9ba93
Zachary Lehmann <zlehmann@gmail.com>
2019-03-26 22:37:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
-  console.log(lis);
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (parseInt(lis.content) * n).toString();
+    lis[i].innerHTML = (parseInt(lis.content) * parseInt(n));
   }
 }
